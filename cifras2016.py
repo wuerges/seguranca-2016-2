975195aee5d5b18ae6d62f35ff65f6824fd926e2
Emilio Wuerges <wuerges@gmail.com>
2016-08-30 17:14:40 -0300

    adicionada cifra de substituição

--- cifras2016.py
+++ cifras2016.py
@@ -5,23 +5,26 @@ from itertools import zip_longest, chain, cycle
 from math import ceil
 from random import shuffle
 
-
+#converts the k argument of a function to an int
 def int_key(func):
     def w(k, d):
         return func(int(k), d)
     return w
 
+#converts the k argument of a function to an array of ints
 def word_key(func):
     def w(k, d):
         return func(k.encode(), d)
     return w
 
-
+# functions for the ceasar cipher
 def ceasar_enc(k, d):
     return bytes((x + k) % 256 for x in d)
 
 def ceasar_dec(k, d):
     return ceasar_enc(k * -1, d)
+
+# functinos for the transposition cipher
 
 def transp_enc(k, d):
     m = [d[i:i+k] for i in range(0, len(d), k)]
@@ -31,6 +34,8 @@ def transp_enc(k, d):
 def transp_dec(k, d):
     kd = ceil(len(d) / k)
     return transp_enc(kd, d)
+
+# functions for the vigenere cipher
 
 def vig_enc(k, d):
     return bytes((b + a) % 256 for (a, b) in zip(cycle(k), d))
