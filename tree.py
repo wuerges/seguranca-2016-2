9a00fb9f85fd9f4cffb6d1a111f724dc1023805f
Emilio Wuerges <wuerges@gmail.com>
2016-09-20 20:27:21 -0300

    Create tree.py

---
+++ tree.py
@@ -0,0 +1,51 @@
+from bitarray import *
+
+def addprefixos(xs, p):
+    return dict((k, bitarray(p) + v) for (k,v) in xs.items())
+
+
+class N:
+    def __init__(self, x, f):
+        self.x = x
+        self.f = f
+        self.l = None
+        self.r = None
+
+    def join(self, o):
+        n = N(None, self.f + o.f)
+        n.l = self
+        n.r = o
+        return n
+
+    def coding(self):
+        l = {}
+        r = {}
+        if self.x:
+            return {self.x: bitarray()}
+        if self.l: 
+            l = self.l.coding()
+        if self.r:
+            r = self.r.coding()
+        ret = {}
+        ret.update(addprefixos(l, '0'))
+        ret.update(addprefixos(r, '1'))
+        return ret
+
+
+
+a = N('a', 0.5)
+b = N('b', 0.1)
+c = N('c', 0.11)
+d = N('d', 0.1)
+
+j1 = a.join(b)
+j2 = c.join(d)
+j3 = j1.join(j2)
+
+print("a:", a.coding())
+print("b:", b.coding())
+print("c:", c.coding())
+print("d:", d.coding())
+print("j1:", j1.coding())
+print("j2:", j2.coding())
+print("j3:", j3.coding())
