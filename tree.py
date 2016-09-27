f725442bd16ad7bc4d677b6172f8b3246c9c0bf0
Emilio Wuerges <wuerges@gmail.com>
2016-09-27 19:58:57 -0300

    huffman

--- tree.py
+++ tree.py
@@ -31,12 +31,19 @@ class N:
         ret.update(addprefixos(r, '1'))
         return ret
 
+    def __lt__(self, o):
+        return self.f > o.f
+
+    def __repr__(self):
+        return "N(%s,%f)" % (self.x, self.f)
 
 
 a = N('a', 0.5)
 b = N('b', 0.1)
 c = N('c', 0.11)
 d = N('d', 0.1)
+
+
 
 j1 = a.join(b)
 j2 = c.join(d)
@@ -49,3 +56,17 @@ print("d:", d.coding())
 print("j1:", j1.coding())
 print("j2:", j2.coding())
 print("j3:", j3.coding())
+
+# l = [a, b, c, d]
+l = [N('a', 4), N(' ', 2), N('c', 1), N('s', 2), N('r', 1), N('o', 1)]
+
+def huffmann(l):
+    while(len(l) > 1):
+        l.sort()
+        a = l.pop()
+        b = l.pop()
+        c = a.join(b)
+        l.append(c)
+    return l.pop().coding()
+
+print(huffmann(l))
